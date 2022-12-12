756763bd1ffcce2c9cd93b0afa1f2390b04344b8
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2022-12-12 17:32:31 -0300

    test

--- frontend/src/utils/getValidationErros.ts
+++ frontend/src/utils/getValidationErros.ts
@@ -1,14 +1,16 @@
 import { ValidationError } from 'yup';
 
-interface Errors{
-    [key: string]: string;
+interface Errors {
+  [key: string]: string;
 }
 
 export default function getValidationErrors(err: ValidationError): Errors {
-    const validationErrors: Errors = {};
+  const validationErrors: Errors = {};
 
-    err.inner.forEach(error => {
-        validationErrors[error.path!] = error.message
-    })
-    return  validationErrors;
+  err.inner.forEach((error) => {
+    if (typeof error.path !== 'undefined') {
+      validationErrors[error.path] = error.message;
+    }
+  });
+  return validationErrors;
 }
