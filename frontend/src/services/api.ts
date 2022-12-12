108adf07b5922d1481e3d4436ce7d2feb19404da
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2022-12-12 20:00:41 -0300

    test

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,4 +4,10 @@ const api = axios.create({
   baseURL: 'https://b50f-177-10-147-105.sa.ngrok.io'
 });
 
+api.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+api.defaults.headers.common['Content'] = 'application/json'
+api.defaults.headers.common['Access-Control-Allow-Methods'] = 'DELETE, PUT, GET, POST'
+api.defaults.headers.common['ngrok-skip-browser-warning'] = 'any'
+
+
 export default api;
