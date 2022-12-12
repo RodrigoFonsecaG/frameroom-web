87aacfaee270937a19d6c126fa07aa3632213a46
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2022-12-12 18:05:02 -0300

    changing api urls

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -15,11 +15,11 @@ const app = express();
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
     res.header(
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept',
     );
-
     next();
 });
 
