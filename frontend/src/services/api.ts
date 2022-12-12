c6daf787027f245ef16df7442014621ef9b18ef2
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2022-12-12 19:11:12 -0300

    test

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://5721-177-10-147-105.sa.ngrok.io'
+  baseURL: 'https://36e1-177-10-147-105.sa.ngrok.io'
 });
 
 export default api;
