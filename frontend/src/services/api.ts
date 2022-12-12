8fad5cded32e8d436ecce8778e10ecc812978b86
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2022-12-12 19:00:45 -0300

    changing api urls

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://88f9-177-10-147-105.sa.ngrok.io'
+  baseURL: 'https://3fb3-177-10-147-105.sa.ngrok.io'
 });
 
 export default api;
